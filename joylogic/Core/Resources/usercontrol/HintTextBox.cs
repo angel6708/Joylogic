5bc10e69e772a0a2f3ce1325f93e4cfa01050211
angel6708 <56676267+angel6708@users.noreply.github.com>
2022-03-27 20:24:07 +0800

    resources

---
+++ joylogic/Core/Resources/usercontrol/HintTextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Resources.usercontrol
+{
+    public class HintTextBox:TextBox
+    {
+        public HintTextBox() 
+        {
+           DefaultStyleKey = typeof(HintTextBox);
+            
+        }
+
+        public static DependencyProperty HintProperty =
+         DependencyProperty.Register("Hint", typeof(string), typeof(HintTextBox), new UIPropertyMetadata("用户名"));
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+
+            set { SetValue(HintProperty, value);
+            }
+            
+
+        }
+
+    }
+}
